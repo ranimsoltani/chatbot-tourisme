2f1fd2bb61818a68158360f6c3e6ebbc04f18906
Maram Temime <maramtemime@gmail.com>
2026-05-05 03:48:58 +0100

    Ajout de la route /chat pour recevoir les messages utilisateur

--- app.py
+++ app.py
@@ -16,3 +16,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.json.get("message")
